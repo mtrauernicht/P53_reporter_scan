c35f979975bda54c343f0785877216419f0ccb1d
mtrauernicht <m.trauernicht@nki.nl>
2023-02-07 15:20:03 +0100

    moved files

--- library_design/barc.r
+++ library_design/barc.r
@@ -1,3 +1,4 @@
+## Script to generate random barcodes
 require(DNABarcodes)
 barc <- create.dnabarcodes(n = 12, dist = 3, filter.triplets = T, metric = "seqlev",filter.gc = T, filter.self_complementary = T, cores = 24)
 write.csv(barc, "barc.csv")
